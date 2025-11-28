53c0e78d701b297092f61c8107b880a6e0170058
Liam Edwards Francisco <franciscoliamedwards@gmail.com>
2025-11-28 13:09:26 +0800

    Update craco file and package libraries

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -5,6 +5,10 @@ module.exports = {
     configure: (config) => {
       config.resolve.fallback = {
         ...config.resolve.fallback,
+        "fs": false, // Ignore fs module
+        "path": false, // Ignore path module
+        "os": false, // Ignore os module
+        "crypto": false, // Ignore crypto module
         buffer: require.resolve("buffer"),
         process: require.resolve("process/browser"),
       };
